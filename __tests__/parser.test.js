4eff24c04a585b196601036f98cf2158e7ef5323
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-03 17:39:01 -0500

    sets up test cases for parser

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,9 +1,26 @@
-import Parser from '../src/types/parser'
-import {SAMPLE, NUMBERS} from './utils/sampleIngredients'
+const {Parser} = require('../src/types/parser')
 
-describe('parses ingredients successfully', () => {
 
-    test('adds numeric amounts to amount object', () => {
-        
+const BASIC_BREAD = `1 package active dry yeast
+3 tablespoons sugar plus 1/2 teaspoon sugar
+2-1/4 cups warm water
+1 tablespoon salt
+6-1/4 to 6-3/4 cups bread flour
+2 tablespoons canola oil`
+
+describe('parser works as expected', () => {
+    const TestParser = new Parser()
+
+    test('given valid recipe input sets a list of sorted ingredients', () => {
+        TestParser.parse(BASIC_BREAD)
+        console.log('THIS IS AFTER ADDED BY PARSER', TestParser.ingredients)
+        expect(TestParser.ingredients?.length).toBe(7)
     })
+
+    test('given a recipe, the first ingredient is returned as expected', () => {
+        const ingredient = TestParser.ingredients[0]
+        expect(ingredient.amount.ml).toBe(1)
+    })
+
+    
 })
--- src/types/parser.ts
+++ src/types/parser.ts
@@ -1,40 +1,44 @@
-// //has current ingredient in constructor
-// //has methods that control this
-// import {Units, UnitsType} from './units'
-// import {Ingredient} from './ingredient'
+//has current ingredient in constructor
+//has methods that control this
+import {Units, UnitsType} from './units'
+import {Ingredient} from './ingredient'
 
 
-// export class Parser{
-//     ingredients: Ingredient[]
-//     currentIngredient: Ingredient
-//     lastIngredient: Ingredient // in case of backtracking
-//     currentWord: string
+export class Parser{
+    ingredients: Ingredient[]
+    currentIngredient: Ingredient
+    lastIngredient: Ingredient // in case of backtracking
+    currentWord: string
 
 
-//     parse(input:string){
-//         const emptyChar = ' ' || '\n'
-//         let i = 0
-//         for (i=0; i<=input.length; i++){
-//             if (input[i] == emptyChar){
-//                 this.currentIngredient.sort(this.currentWord)
-//                 this.currentWord = ''
-//             } else {
-//                 this.currentWord += input[i]
-//             }
-//         }
+    parse(input:string){
+        const emptyChar = ' ' || '\n' || '-'
+        let i = 0
+        for (i=0; i<=input.length; i++){
+            if (input[i] == emptyChar){
+                if (!this.currentIngredient) this.currentIngredient = new Ingredient()
+                this.currentIngredient.sort(this.currentWord)
+                this.currentWord = ''
+                this.addCurrentToIngredients()
+            } else {
+                this.currentWord += input[i]
+            }
+        }
 
-//     }
+        if (this.currentIngredient) this.addCurrentToIngredients()
+    }
 
-//     addCurrentToIngredients(){
-//         if (this.currentIngredient.isWholeIngredient()){
-//             this.ingredients = this.ingredients.push(this.currentIngredient)
-//             this.currentIngredient = new Ingredient
-//         } 
-//     }
+    addCurrentToIngredients(){
+        if (!this.ingredients) this.ingredients = []
+        if (this.currentIngredient.isValidIngredient()){
+            this.ingredients = [...this.ingredients, this.currentIngredient]
+            this.currentIngredient = new Ingredient
+        } 
+    }
 
 
 
-// }
+}
 
 
 
