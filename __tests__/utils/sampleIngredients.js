15004a80ca8e05779dcd45e8b8f788a56c6f27c9
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-15 08:45:49 -0500

    removes unused test utils

--- __tests__/utils/sampleIngredients.js
+++
@@ -1,8 +0,0 @@
-export const SAMPLE = [
-    '5 eggs',
-    '1 1/2 cups flour', //fraction split by words
-    '1.3 cup water', // decimal unit ingredient
-    '6-1/4 to 6-3/4 cups bread flour' // fraction split by hyphen
-]
-
-
