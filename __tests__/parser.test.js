e7ae2567d3aa0dd8ee14c55537dcb565506e277e
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-18 19:33:03 -0500

    adds comments for where to go next with parser TDD

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,26 +1,39 @@
-const {Parser} = require('../src/types/parser')
-
+const { Parser } = require("../src/types/parser");
+const { Units } = require("../src/types/units");
 
 const BASIC_BREAD = `1 package active dry yeast
 3 tablespoons sugar plus 1/2 teaspoon sugar
 2-1/4 cups warm water
 1 tablespoon salt
 6-1/4 to 6-3/4 cups bread flour
-2 tablespoons canola oil`
+2 tablespoons canola oil`;
 
-describe('parser works as expected', () => {
-    const TestParser = new Parser()
+describe("parser works as expected", () => {
+  const TestParser = new Parser();
 
-    test('given valid recipe input sets a list of sorted ingredients', () => {
-        TestParser.parse(BASIC_BREAD)
-        console.log('THIS IS AFTER ADDED BY PARSER', TestParser.ingredients)
-        expect(TestParser.ingredients?.length).toBe(7)
-    })
+  test("given valid recipe input sets a list of sorted ingredients", () => {
+    TestParser.parse(BASIC_BREAD);
+    console.log("THIS IS AFTER ADDED BY PARSER", TestParser.ingredients);
+    expect(TestParser.ingredients?.length).toBe(7);
+  });
 
-    test('given a recipe, the first ingredient is returned as expected', () => {
-        const ingredient = TestParser.ingredients[0]
-        expect(ingredient.amount.ml).toBe(1)
-    })
+  test("given a recipe, the first ingredient is returned as expected", () => {
+    const ingredient = TestParser.ingredients[0];
+    expect(ingredient.amount.ml).toBe(1);
+    expect(ingredient.unit).toBeNull();
+    expect(ingredient.ingredient.name).toBe("package active dry yeast");
+  });
 
-    
-})
+  // does it capture all units??
+  test("processes all valid units as valid", () => {
+    Object.keys(Units).map();
+  });
+
+  // does it capture last item or leave it hanging??
+
+  // does it excessively split string string string etc
+
+  // does it backtrack if faced with a case??
+
+  // does it error out if there are invalid chars?
+});
--- src/types/parser.ts
+++ src/types/parser.ts
@@ -1,54 +1,47 @@
 //has current ingredient in constructor
 //has methods that control this
-import {Units, UnitsType} from './units'
-import {Ingredient} from './ingredient'
+import { Units, UnitsType } from "./units";
+import { Ingredient } from "./ingredient";
 
+export class Parser {
+  ingredients: Ingredient[];
+  currentIngredient: Ingredient;
+  lastIngredient: Ingredient; // in case of backtracking
+  currentWord: string;
 
-export class Parser{
-    ingredients: Ingredient[]
-    currentIngredient: Ingredient
-    lastIngredient: Ingredient // in case of backtracking
-    currentWord: string
-
-
-    parse(input:string){
-        const emptyChar = ' ' || '\n' || '-'
-        let i = 0
-        for (i=0; i<=input.length; i++){
-            if (input[i] == emptyChar){
-                if (!this.currentIngredient) this.currentIngredient = new Ingredient()
-                this.currentIngredient.sort(this.currentWord)
-                this.currentWord = ''
-                this.addCurrentToIngredients()
-            } else {
-                this.currentWord += input[i]
-            }
-        }
-
-        if (this.currentIngredient) this.addCurrentToIngredients()
+  parse(input: string) {
+    const emptyChar = " " || "\n" || "-";
+    let i = 0;
+    for (i = 0; i <= input.length; i++) {
+      if (input[i] == emptyChar) {
+        if (!this.currentIngredient) this.currentIngredient = new Ingredient();
+        this.currentIngredient.sort(this.currentWord);
+        this.currentWord = "";
+        this.addCurrentToIngredients();
+      } else {
+        this.currentWord += input[i];
+      }
     }
 
-    addCurrentToIngredients(){
-        if (!this.ingredients) this.ingredients = []
-        if (this.currentIngredient.isValidIngredient()){
-            this.ingredients = [...this.ingredients, this.currentIngredient]
-            this.currentIngredient = new Ingredient
-        } 
+    if (this.currentIngredient) this.addCurrentToIngredients();
+  }
+
+  addCurrentToIngredients() {
+    if (!this.ingredients) this.ingredients = [];
+    // validates and adds
+    if (this.currentIngredient.isValidIngredient()) {
+      this.ingredients = [...this.ingredients, this.currentIngredient];
+      // replaces with blank ingredient
+      this.currentIngredient = new Ingredient();
     }
-
-
-
+  }
 }
 
-
-
-
-
 // ingredient
-    // sort
-        // is unit
-        // set unit
-        // is digit
-        // format number
-        // set digit
-        // set ingredient
+// sort
+// is unit
+// set unit
+// is digit
+// format number
+// set digit
+// set ingredient
