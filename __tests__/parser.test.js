7597f10cc43b65071b6faaacb3bc8c471618f8da
Rachel Reilly <r.a.reill18@gmail.com>
2021-11-07 13:33:17 -0600

    tests first two ingredients for expected ingredient

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -2,34 +2,30 @@ const { Parser } = require("../dist/types/parser");
 const { Units } = require("../dist/types/units");
 
 const BASIC_BREAD = `1 package active dry yeast
-3 tablespoons sugar plus 1/2 teaspoon sugar
+3 tablespoons sugar 
 2-1/4 cups warm water
 1 tablespoon salt
-6-1/4 to 6-3/4 cups bread flour
+6-1/4 cups bread flour
 2 tablespoons canola oil`;
 
-describe("parser works as expected", () => {
+describe("Parser class parses input data correctly", () => {
   const TestParser = new Parser();
+  const recipe = TestParser.parse(BASIC_BREAD);
 
-  test("given valid recipe input sets a list of sorted ingredients", () => {
-    TestParser.parse(BASIC_BREAD);
-    expect(TestParser.ingredients?.length).toBe(7);
+  test("given valid input with a given number of ingredients, it returns an array of ingredients with that length", () => {
+    expect(recipe.ingredients?.length).toBe(6);
   });
 
-  // test("given a recipe, the first ingredient is returned as expected", () => {
-  //   const ingredient = TestParser.ingredients[0];
-  //   expect(ingredient.amount.amount).toBe(1);
-  //   expect(ingredient.unit).toBeNull();
-  //   expect(ingredient.ingredient.name).toBe("package active dry yeast");
-  // });
+  test("given a valid recipe input, it parses ingredients correctly and in order", () => {
+    const firstIngredient = recipe.ingredients[0];
+    const secondIngredient = recipe.ingredients[1];
 
-  // does it capture all units??
+    expect(firstIngredient.amount.amount).toBe(1);
+    expect(firstIngredient.unit).toBeNull();
+    expect(firstIngredient.ingredient.name).toBe("package active dry yeast");
 
-  // does it capture last item or leave it hanging??
-
-  // does it excessively split string string string etc
-
-  // does it backtrack if faced with a case??
-
-  // does it error out if there are invalid chars?
+    expect(secondIngredient.amount.amount).toBe(3);
+    expect(secondIngredient.unit.name.short).toBe(Units.tablespoon.name.short);
+    expect(secondIngredient.ingredient.name).toBe("sugar");
+  });
 });
