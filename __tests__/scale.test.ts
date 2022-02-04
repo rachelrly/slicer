f0dc824cd27bca6bee2fae19607951211795908b
Rachel <rachelreillydev@gmail.com>
2022-02-04 10:12:54 -0600

    adds tests for locked property

--- __tests__/scale.test.ts
+++ __tests__/scale.test.ts
@@ -2,6 +2,8 @@
 // import { Recipe } from "../src/types/recipe";
 // import { Units } from "../src/types/units";
 
-describe.skip('It parses a recipe from a string and multiplies it by a constant', () => {
-  expect(1).toBe(1)
+describe('It satisfies Jest with a test', () => {
+  test('It satisfies Jest with a test', () => {
+    expect(1).toBe(1)
+  })
 })
