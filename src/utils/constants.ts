5d096ee1de16221f0e56bd2f31f5a9a91e8e07a5
Rachel <rachelreillydev@gmail.com>
2022-02-08 18:12:44 -0600

    moves FRACTION object to /constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,5 +9,37 @@ export const BREAK_ON_CHAR = /[\s|-]/
 // Keeps alphabetical chars, digits, '-', '/', '-', '%'
 export const REPLACE_CHAR = /(?![\-|\.|\/\\|\%])(\W)/g
 
-// This is not being used in favor of the simple /\d/
-// export const DIGIT = /^[\d|\.|\/]+$/
+// Fractions use string matching and a range as opposed to calculated
+//  fractions because I couldn't figure out a clean way to handle
+//  reduced fractions and because there is a fininte amount of
+//  useful fractions in cooking
+export const FRACTIONS = {
+  '0': {
+    min: 0,
+    max: 0.11
+  },
+  '1/4': {
+    min: 0.12,
+    max: 0.28
+  },
+  '1/3': {
+    min: 0.29,
+    max: 0.43
+  },
+  '1/2': {
+    min: 0.44,
+    max: 0.58
+  },
+  '2/3': {
+    min: 0.59,
+    max: 0.7
+  },
+  '3/4': {
+    min: 0.71,
+    max: 0.85
+  },
+  '1': {
+    min: 0.86,
+    max: 1
+  }
+}
--- src/utils/formatAmount.ts
+++ src/utils/formatAmount.ts
@@ -1,38 +1,4 @@
-// This uses string matching as opposed to calculations
-//  because I couldn't figure out a clean way to handle
-//  reduced fractions for repeating decimals and rounding
-//  and because there is a fininte amount of useful fractions in cooking
-
-export const FRACTIONS = {
-  '0': {
-    min: 0,
-    max: 0.11
-  },
-  '1/4': {
-    min: 0.12,
-    max: 0.28
-  },
-  '1/3': {
-    min: 0.29,
-    max: 0.43
-  },
-  '1/2': {
-    min: 0.44,
-    max: 0.58
-  },
-  '2/3': {
-    min: 0.59,
-    max: 0.7
-  },
-  '3/4': {
-    min: 0.71,
-    max: 0.85
-  },
-  '1': {
-    min: 0.86,
-    max: 1
-  }
-}
+import { FRACTIONS } from './constants'
 
 export function formatAmount(amount: number, format = true): string {
   const string = formatAmountString(amount)
